fa7bab07592ad2d9e91c83479df905f3ef00eb11
Brizanac <45972866+Brizanac@users.noreply.github.com>
2019-01-04 16:25:35 +0100

    Add files via upload

---
+++ sluzba dostave/sluzba dostave/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sluzba_dostave
+{
+    public partial class Form1 : Form
+    {
+        public string username, password;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sr = new System.IO.StreamReader("C:\\" + textBox1.Text + "\\login.ID");
+                username = sr.ReadLine();
+                password = sr.ReadLine();
+                sr.Close();
+
+                if (username == textBox1.Text && password == textBox2.Text)
+                    MessageBox.Show("Uspješno ste se prijavili!", "Uspješna prijava!");
+                else
+                    MessageBox.Show("Pogrešno ste unijeli korisničko ime ili lozinku!", "Pogreška!");
+
+            }
+            catch (System.IO.DirectoryNotFoundException ex)
+            {
+                MessageBox.Show("Korisnik ne postoji!", "Pogreška!");
+
+            }
+        }
+    }
+}
---
+++ sluzba dostave/sluzba dostave/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO; 
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sluzba_dostave
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           this.Close(); 
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var sw = new System.IO.StreamWriter("C:\\" + textBox1.Text + "\\login.ID");
+                sw.Write(textBox1.Text + "\n" + textBox2.Text);
+                sw.Close();
+
+            }
+            catch (System.IO.DirectoryNotFoundException ex)
+            {
+                System.IO.Directory.CreateDirectory("C:\\" + textBox1.Text);
+                var sw = new System.IO.StreamWriter("C:\\" + textBox1.Text + "\\login.ID");
+                sw.Write(textBox1.Text + "\n" + textBox2.Text);
+                sw.Close();
+            }
+
+            MessageBox.Show("Korisnik spremljen!", "Uspješno!");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
